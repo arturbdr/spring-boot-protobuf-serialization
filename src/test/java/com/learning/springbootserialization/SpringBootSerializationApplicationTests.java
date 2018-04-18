172d2662f8325d8fffeb6dcbad0fc7b8654deb23
Artur Drummond <arturbdr@gmail.com>
2018-04-17 23:05:45 -0300

    - initial commit

---
+++ src/test/java/com/learning/springbootserialization/SpringBootSerializationApplicationTests.java
@@ -0,0 +1,14 @@
+package com.learning.springbootserialization;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootSerializationApplicationTests {
+
+  @Test
+  public void contextLoads() {}
+}
